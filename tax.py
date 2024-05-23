30c1499b8067a745bc9886206746caf178edd01a
Jialing Liu <jialingl.liu@gmail.com>
2024-05-23 12:08:18 +0200

    modified tax page and created weather tracking page

--- tax.py
+++ tax.py
@@ -2,3 +2,6 @@ amount = 100
 tax = 0.06
 total = amount + amount*tax
 print(total) 
+
+total = int(amount + amount*tax)
+print(total)
---
+++ weather.py
@@ -0,0 +1,7 @@
+temp = 80
+raining = True
+
+if temp < 80 or raining:
+    print('Bad weather. Stay inside!')
+else: 
+    print ("Enjoy the day!")

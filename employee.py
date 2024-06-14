32ad9b4446325809f58a8d184cb2292d35e43ddb
Jialing Liu <jialingl.liu@gmail.com>
2024-06-14 12:15:49 -0400

    converted employee as parent class and created other child classes

--- employee.py
+++ employee.py
@@ -1,15 +1,29 @@
 #this class has relation with the company class file
 
+#general employee class so that all types of employees can inherit its properties and not just the salaried ones.
 class Employee:
-    def __init__ (self, fname, lname, salary):
+    def __init__(self, fname, lname):
         self.fname = fname
         self.lname = lname
+        
+#this class became the child class and now it derives/inherit from the Employee class.
+class SalaryEmployee(Employee):
+    def __init__ (self, fname, lname, salary):
+        #self.fname = fname -NO longer needed since now it inherit these from elsewhere.
+        #self.lname = lname
+        super().__init__(fname, lname) #--use super method to pass in properties from parent class
         self.salary = salary
 
     #52 weeks of the year
     def calculate_paycheck(self):
         return self.salary/52
         
-#sample1 = Employee('Sara', 'Lee', 5000)
-#print(sample1.fname)
+class HourlyEmployee(Employee):
+    def __init__(self, fname, lname, weekly_hours, hourly_rate):
+        super().__init__(fname, lname)
+        self.weekly_hours = weekly_hours
+        self.hourly_rate = hourly_rate
+    
+    def calculate_paycheck(self):
+        return self.weekly_hours*self.hourly_rate
         

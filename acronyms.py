aec8d97db40788b780162c6dd7910f7b61a8a42e
Jialing Liu <jialingl.liu@gmail.com>
2025-02-15 15:36:32 -0500

    wrapped find and add acronym into functions

--- acronyms.py
+++ acronyms.py
@@ -1,12 +1,27 @@
-# ask the user what acronym they want
-acronym = input("What acronym do you want to add?\n")
+def find_acronym():
+    look_up = input("What acronym would you like to look up?\n")
 
-#ask user for the definition
-definition = input("What is the definition?\n")
+    found = False
+    with open('acronyms.txt') as file:
+        for line in file:
+            if look_up in line:
+                print(line)
+                found = True
+                break
+    if not found:
+        print("The acronym does not exist")
 
 
-#open the file
-with open('acronyms.txt', 'a') as file:
-    file.write(acronym + ' - ' + definition + '\n')
+def add_acronym():
+    # ask the user what acronym they want
+    acronym = input("What acronym do you want to add?\n")
 
-# write the new acronym and definition to the file
+    #ask user for the definition
+    definition = input("What is the definition?\n")
+
+
+    #open the file
+    with open('acronyms.txt', 'a') as file:
+        file.write(acronym + ' - ' + definition + '\n')
+
+    # write the new acronym and definition to the file
